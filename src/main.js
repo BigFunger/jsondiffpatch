96b2742f3241ca99a68e8133571ebb7fe8507604
Jim Unger <BigFunger@gmail.com>
2016-02-03 09:29:40 -0600

    removed expression-based paths from require statement

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-
 var environment = require('./environment');
+require('../public/formatters-styles/annotated.css');
+require('../public/formatters-styles/html.css');
 
 var DiffPatcher = require('./diffpatcher').DiffPatcher;
 exports.DiffPatcher = DiffPatcher;
 
 exports.create = function(options){
-	return new DiffPatcher(options);
+  return new DiffPatcher(options);
 };
 
 exports.dateReviver = require('./date-reviver');
@@ -13,45 +14,43 @@ exports.dateReviver = require('./date-reviver');
 var defaultInstance;
 
 exports.diff = function() {
-	if (!defaultInstance) {
-		defaultInstance = new DiffPatcher();
-	}
-	return defaultInstance.diff.apply(defaultInstance, arguments);
+  if (!defaultInstance) {
+    defaultInstance = new DiffPatcher();
+  }
+  return defaultInstance.diff.apply(defaultInstance, arguments);
 };
 
 exports.patch = function() {
-	if (!defaultInstance) {
-		defaultInstance = new DiffPatcher();
-	}
-	return defaultInstance.patch.apply(defaultInstance, arguments);
+  if (!defaultInstance) {
+    defaultInstance = new DiffPatcher();
+  }
+  return defaultInstance.patch.apply(defaultInstance, arguments);
 };
 
 exports.unpatch = function() {
-	if (!defaultInstance) {
-		defaultInstance = new DiffPatcher();
-	}
-	return defaultInstance.unpatch.apply(defaultInstance, arguments);
+  if (!defaultInstance) {
+    defaultInstance = new DiffPatcher();
+  }
+  return defaultInstance.unpatch.apply(defaultInstance, arguments);
 };
 
 exports.reverse = function() {
-	if (!defaultInstance) {
-		defaultInstance = new DiffPatcher();
-	}
-	return defaultInstance.reverse.apply(defaultInstance, arguments);
+  if (!defaultInstance) {
+    defaultInstance = new DiffPatcher();
+  }
+  return defaultInstance.reverse.apply(defaultInstance, arguments);
 };
 
 if (environment.isBrowser) {
-	exports.homepage = '{{package-homepage}}';
-	exports.version = '{{package-version}}';
+  exports.homepage = '{{package-homepage}}';
+  exports.version = '{{package-version}}';
 } else {
-	var packageInfoModuleName = '../package.json';
-	var packageInfo = require(packageInfoModuleName);
-	exports.homepage = packageInfo.homepage;
-	exports.version = packageInfo.version;
-
-	var formatterModuleName = './formatters';
-	var formatters = require(formatterModuleName);
-	exports.formatters = formatters;
-	// shortcut for console
-	exports.console = formatters.console;
+  var packageInfo = require('../package.json');
+  exports.homepage = packageInfo.homepage;
+  exports.version = packageInfo.version;
 }
+
+var formatters = require('./formatters');
+exports.formatters = formatters;
+// shortcut for console
+exports.console = formatters.console;
